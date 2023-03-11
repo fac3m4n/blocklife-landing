8135019e4559155fda4b1099554fd7498dad58ac
Kerim <kerimberdi.agayev@edusoftech.eu>
2023-03-12 00:33:22 +0200

    png bug fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../public/img/block_logo.png";
+import logo from "../public/img/logoblack.png";
 
 const Navbar = () => {
   return (
