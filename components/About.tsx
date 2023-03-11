daa1e9c32bda4498514843812e440dd548d5b97b
Kerim <kerimberdi.agayev@edusoftech.eu>
2023-03-11 17:10:54 +0200

    tech stack and assets

--- components/About.tsx
+++ components/About.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import React from "react";
 import blockDice from "../public/img/dice_game.png";
+import { GiRollingDices } from "react-icons/gi";
+import { TbArrowUpRight } from "react-icons/tb";
 
 const About = () => {
   return (
     <section className="py-10">
-      <div className="max-w-5xl mx-auto px-6 lg:px-8 flex flex-col lg:flex-row items-center justify-center">
+      <div className="max-w-7xl mx-auto px-6 lg:px-8 flex flex-col lg:flex-row items-center justify-center">
         <div className="lg:w-1/2">
           <Image
             className="rounded-lg shadow-lg w-full object-cover"
@@ -16,15 +18,19 @@ const About = () => {
           />
         </div>
         <div className="lg:w-1/2 lg:pl-8 mt-10 lg:mt-0">
-          <h2 className="text-4xl font-bold text-gray-800 mb-4">
+          <h2 className="text-4xl font-bold text-gray-800 mb-4 flex items-center">
             About BlockDice
+            <GiRollingDices size={56} className="fill-funcia" />
           </h2>
           <p className="text-lg leading-relaxed">
             BlockDice is an on-chain turn-based game. It is a decentralized
             version of popular diced-table Monopoly game. The game is played
             between two and ten players, each of which has a wallet.
           </p>
-          <button className="underline">Learn More</button>
+          <button className="mt-6 flex items-center hover:text-funcia">
+            <p className="text-2xl">Learn More</p>
+            <TbArrowUpRight size={32} />
+          </button>
         </div>
       </div>
     </section>
--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -1,41 +1,82 @@
 import React from "react";
+import Image from "next/image";
+// Front
+import nextjs from "../public/img/nextjs.png";
+import ts from "../public/img/ts.png";
+import webgl from "../public/img/webgl.png";
+import tailwind from "../public/img/tailwind.png";
+
+// Back
+import unity from "../public/img/unit.png";
+import csharp from "../public/img/csharp.png";
+import nether from "../public/img/nethereum.webp";
+
+// Blockchain
+import polygon from "../public/img/polygon.png";
+import hardhat from "../public/img/hardhat.png";
+import metamask from "../public/img/metamask.png";
+import thirdweb from "../public/img/thirdweb.svg";
+
+import ether from "../public/img/ether.png";
+import openzep from "../public/img/openzep.png";
 
 const TechStack = () => {
   return (
-    <section className="bg-gray-100 py-10">
-      <div className="max-w-5xl mx-auto px-6 lg:px-8">
+    <section className="py-10">
+      <div className="max-w-7xl mx-auto px-6 lg:px-8">
         <h2 className="text-4xl font-bold text-gray-800 mb-8 text-center">
           Our Tech Stack
         </h2>
         <div className="flex flex-wrap -mx-4">
-          <div className="w-full md:w-1/3 px-4 mb-8">
+          <div className="w-full md:w-1/4 px-4 mb-8">
             <div className="bg-white rounded-lg overflow-hidden shadow-lg p-6">
               <h3 className="text-xl font-bold text-gray-800 mb-2">Frontend</h3>
-              <ul className="text-gray-600">
-                <li>React</li>
-                <li>Vue.js</li>
-                <li>Angular</li>
-              </ul>
+              <div className="grid grid-cols-2 gap-x-2 gap-y-4 items-center content-center">
+                <Image src={ts} alt="tech" height={56} />
+                <Image src={nextjs} alt="tech" height={56} />
+                <Image src={webgl} alt="tech" height={56} />
+                <Image src={tailwind} alt="tech" height={56} />
+              </div>
             </div>
           </div>
-          <div className="w-full md:w-1/3 px-4 mb-8">
+          <div className="w-full md:w-1/4 px-4 mb-8">
             <div className="bg-white rounded-lg overflow-hidden shadow-lg p-6">
               <h3 className="text-xl font-bold text-gray-800 mb-2">Backend</h3>
-              <ul className="text-gray-600">
-                <li>Node.js</li>
-                <li>Express.js</li>
-                <li>Ruby on Rails</li>
-              </ul>
+              <div className="grid grid-cols-2 gap-x-2 gap-y-4 items-center">
+                <Image src={unity} alt="tech" height={56} />
+                <Image src={csharp} alt="tech" height={56} />
+                <Image src={nether} alt="tech" height={56} />
+              </div>
             </div>
           </div>
-          <div className="w-full md:w-1/3 px-4 mb-8">
+          <div className="w-full md:w-1/4 px-4 mb-8">
             <div className="bg-white rounded-lg overflow-hidden shadow-lg p-6">
-              <h3 className="text-xl font-bold text-gray-800 mb-2">Database</h3>
-              <ul className="text-gray-600">
-                <li>MySQL</li>
-                <li>PostgreSQL</li>
-                <li>MongoDB</li>
-              </ul>
+              <h3 className="text-xl font-bold text-gray-800 mb-2">
+                Blockcain
+              </h3>
+              <div className="grid grid-cols-2 gap-x-2 gap-y-4 items-center">
+                <Image src={polygon} alt="tech" height={56} />
+                <Image src={hardhat} alt="tech" height={56} />
+                <Image src={metamask} alt="tech" height={56} />
+                <Image src={thirdweb} alt="tech" height={56} />
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/4 px-4 mb-8">
+            <div className="bg-white rounded-lg overflow-hidden shadow-lg p-6">
+              <h3 className="text-xl font-bold text-gray-800 mb-2">
+                Token Standards
+              </h3>
+              <div className="grid grid-cols-2 gap-x-2 gap-y-4 items-center font-bold">
+                <div className="flex items-center space-x-2">
+                  <Image src={ether} alt="tech" height={50} />
+                  ERC-1155
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Image src={ether} alt="tech" height={50} />
+                  ERC-4337
+                </div>
+              </div>
             </div>
           </div>
         </div>
