e1d17209fe587e00583d70d72417bc237bef3a92
Kerim <kerimberdi.agayev@edusoftech.eu>
2023-03-12 11:38:37 +0200

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Image from "next/image";
+import salty from "../public/img/saltyNode.png";
 
 const Footer = () => {
   return (
     <footer className="bg-primary py-4">
-      <div className="max-w-5xl mx-auto px-6 lg:px-8 text-center text-sm">
-        &copy; 2023 Bucharest Hackathon. Salty Node
+      <div className="flex items-center justify-center max-w-5xl mx-auto px-6 lg:px-8 text-center text-md space-x-2">
+        <div>&copy; 2023 Bucharest Hackathon. Salty Node</div>
+        <Image src={salty} alt="salty" height={32} />
       </div>
     </footer>
   );
